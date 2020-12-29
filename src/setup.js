4d7217172777cca622a5c7840d1fcecab81c0738
GhostSD <ghostsoftwaredevelopment@gmail.com>
2020-12-29 14:03:42 +0100

    updating the most important information about the project

--- src/setup.js
+++ src/setup.js
@@ -1,16 +1,21 @@
+// MongoDB Realm setup
+
 export const APP_ID = "aasite-sumns";
 export const DB_NAME = "ArchonSite";
 
+// Collections configuration
 export const Collections = {
 	CARDS: "card",
 	CALENDAR: "calendar",
 };
 
+// Internal path identyficators
 export const Path = {
 	PARENT: "..",
 	DELIMITER: "\\",
 };
 
+// Internal paths
 export const Paths = {
 	Calendar: Path.DELIMITER + "calendar",
 };
