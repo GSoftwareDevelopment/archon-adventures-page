de7829b8a4856268ed9cdabab85fd54558a2db14
GhostSD <ghostsoftwaredevelopment@gmail.com>
2021-01-05 15:50:08 +0100

    Code refactoring to introduce new libraries.

--- src/components/page-elements/Card.jsx
+++ src/components/page-elements/Card.jsx
@@ -3,42 +3,21 @@ import { db } from "../../libs/db";
 import { Collections } from "../../setup";
 import { observer } from "mobx-react";
 import LayoutsStore from "../../store/layouts";
-import { pathDestructure } from "../../libs/utils";
+import { languageCheck, pathDestructure } from "../../libs/utils";
 
 import "../layout/card.scss";
+import * as Messages from "../layout/Messages";
 
 import MarkdownView from "react-showdown";
+import { showdown_ext, showdown_options } from "./Card-markdown-ext.js";
 import ContentLoader from "../layout/ContentLoader";
-import { InfoSquare as IconInfoSquare } from "react-bootstrap-icons";
-import * as BSIcon from "react-bootstrap-icons";
+import {
+	InfoSquare as IconInfoSquare,
+	JournalX as IconJournalX,
+} from "react-bootstrap-icons";
 
 import CardEdit /*, { CardEditControl } */ from "../pages/Card-Edit";
 
-// showdown options
-const showdown_options = {
-	tables: true,
-	emoji: true,
-	parseImgDimensions: true,
-	literalMidWordUnderscores: true,
-	strikethrough: true,
-	simpleLineBreaks: true,
-	openLinksInNewWindow: true,
-	metadata: true,
-};
-
-//
-
-function Icon({ ...props }) {
-	const BootstrapIcon = BSIcon[props.name];
-	if (BootstrapIcon) return <BootstrapIcon {...props} />;
-	else return null;
-}
-
-function Align({ children, ...props }) {
-	return (
-		<span style={{ display: "block", textAlign: props.to }}>{children}</span>
-	);
-}
 //
 
 class Card extends Component {
@@ -46,8 +25,7 @@ class Card extends Component {
 		super(props);
 		this.state = {
 			card: null,
-			currentLang: props.lang || "",
-			content: "",
+			choicedLang: "",
 			isReading: false,
 			isEdit: false,
 		};
@@ -92,7 +70,7 @@ class Card extends Component {
 				isReading: false,
 			});
 		} else {
-			console.warn("Content not found for search parameters: ", find);
+			Messages.toConsole("debug.card.fetchError.contentNotFound", find);
 			this.setState({
 				card: null,
 				content: `Can't load content. Check console log for more details :(`,
@@ -100,6 +78,10 @@ class Card extends Component {
 			});
 		}
 	}
+
+	changeLang = (lang) => {
+		Messages.toConsole("debug.card.userLangChoice", lang);
+	};
 
 	updateContent = (cardData) => {
 		this.setState({ card: cardData });
@@ -118,52 +100,92 @@ class Card extends Component {
 			return <ContentLoader busy={this.state.isReading} />;
 		if (!this.state.card) return null;
 
-		const cardLangs = this.state.card.lang;
+		const card = this.state.card;
+
+		Messages.toConsole("debug.card.render.open", card.name);
+
+		const cardLangs = [];
+		for (const lang in card.body) {
+			cardLangs.push(lang);
+		}
+		Messages.toConsole("debug.card.render.bodyLanguages", cardLangs);
 
 		const defaultLang = LayoutsStore.getDefaultLang();
-		let currentLang = LayoutsStore.getCurrentLang();
+		const currentLang = LayoutsStore.getCurrentLang();
+		let langChange;
 
-		let content = this.getContentByLang(currentLang);
-		let langChange = false;
-		if (!content) {
-			console.log("Card ", this.state.card.name);
-			console.log(
-				"- The content of this card is not defined in the selected language."
+		const usedLang = languageCheck(
+			currentLang,
+			defaultLang,
+			cardLangs,
+			(lang) => {
+				return this.getContentByLang(lang) !== "";
+			}
+		);
+
+		let content;
+		if (usedLang) {
+			langChange = usedLang !== currentLang;
+			content = this.getContentByLang(usedLang);
+			if (langChange) Messages.toConsole("langWarning", usedLang, currentLang);
+		} else {
+			Messages.toConsole("noLangContext");
+			return (
+				<div className="warning">
+					<IconJournalX size="32" />
+					<MarkdownView
+						markdown={Messages.getText("noLangContext", currentLang)}
+					/>
+				</div>
 			);
-
-			langChange = true;
-			if (currentLang !== defaultLang) {
-				currentLang = defaultLang;
-				content = this.getContentByLang(currentLang);
-			}
-			if (!content) {
-				cardLangs.forEach((lang) => {
-					content = this.getContentByLang(lang);
-					currentLang = lang;
-					if (content) return true;
-				});
-			}
-			if (!content) {
-				console.warn("! No content found");
-				return <div>No content :(</div>;
-			}
-			console.log("- Language of content was changed to: ", currentLang);
 		}
+
+		if (card.options) {
+			Messages.toConsole("debug.card.render.optionsModified.open");
+			card.options.forEach((option) => {
+				if (typeof option === "string") {
+					Messages.toConsole(
+						"debud.card.render.optionsModfied.flagSet",
+						option
+					);
+					switch (option) {
+						case "noLangWarnings":
+							langChange = false;
+							break;
+						default:
+							Messages.toConsole(
+								"debug.card.render.optionsModified.badOption",
+								option
+							);
+							console.log(`Option '${option}' is not recognized`);
+					}
+					return;
+				}
+			});
+			Messages.toConsole("debug.card.render.optionsModified.close");
+		}
+
+		Messages.toConsole("debug.card.render.close", card.name);
+
 		return (
 			<React.Fragment>
 				{langChange && (
-					<LangWarning cardLangs={cardLangs} currentLang={currentLang} />
+					<LangWarning
+						cardLangs={cardLangs}
+						currentLang={usedLang}
+						onLangChange={this.changeLang}
+					/>
 				)}
 				<MarkdownView
-					key={this.state.currentLang}
+					key={usedLang}
 					className="markdown"
 					markdown={content}
 					options={showdown_options}
-					components={{ Icon, Align }}
+					components={showdown_ext}
 				/>
 				{this.state.isEdit && (
 					<CardEdit
-						card={this.state.card}
+						card={card}
 						onUpdate={this.updateContent}
 						onClose={this.hideEdit}
 					/>
@@ -176,15 +198,16 @@ class Card extends Component {
 function LangWarning(props) {
 	if (!props.cardLangs || !props.cardLangs.length === 0) return null;
 	const availableLangs = LayoutsStore.getAvailableLang();
+	const preferedLang = LayoutsStore.getCurrentLang();
 
 	return (
-		<div id="lang-warning">
+		<div className="warning">
 			<IconInfoSquare size="32" />
 			<div>
-				<p>This article is not available in the language of your choice.</p>
+				<MarkdownView
+					markdown={Messages.getText("langWarning", preferedLang)}
+				/>
 				<p>
-					Language versions available:
-					<br />
 					{props.cardLangs.map((symbol) => {
 						const lang = availableLangs.find((lang) => lang.symbol === symbol);
 
@@ -192,6 +215,9 @@ function LangWarning(props) {
 							<span
 								key={lang.symbol}
 								className={lang.symbol === props.currentLang ? "current" : ""}
+								onClick={() => {
+									props.onLangChange(lang.symbol);
+								}}
 							>
 								{lang.name}
 							</span>
