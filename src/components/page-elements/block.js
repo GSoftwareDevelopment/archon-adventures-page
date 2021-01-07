a98b8fe6d3238fafc2c92c4e9359c555ce8d1671
GhostSD <ghostsoftwaredevelopment@gmail.com>
2021-01-07 13:31:02 +0100

    Collective libraries for block and contextual Layout elements

---
+++ src/components/page-elements/block.js
@@ -0,0 +1,6 @@
+import Header from "./Header";
+import MenuRouter from "./MenuRouter";
+import RouterContent from "./RouterContent";
+import Footer from "./Footer";
+
+export { Header, MenuRouter, RouterContent, Footer };
---
+++ src/components/page-elements/content.js
@@ -0,0 +1,6 @@
+import MenuLink from "./MenuLink";
+import MenuLangSelector from "./MenuLangSelector";
+import Calendar from "./Calendar";
+import Card from "./Card";
+
+export { MenuLangSelector, MenuLink, Calendar, Card };
