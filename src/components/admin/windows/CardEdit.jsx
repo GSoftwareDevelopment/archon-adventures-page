58ad0720e72b83c6aa7183260bb04333df724528
GhostSD <ghostsoftwaredevelopment@gmail.com>
2021-01-15 08:01:16 +0100

    Components of administrative dialogs

---
+++ src/components/admin/windows/CardEdit.jsx
@@ -0,0 +1,114 @@
+import React, { Component } from "react";
+import { db } from "../../../libs/db";
+import { Collections } from "../../../setup";
+import LayoutsStore from "../store/layouts";
+
+import Window from "./Window";
+
+const status = {
+	INIT: "init",
+	READING: "reading",
+	DONE: "done",
+	ERROR: "error",
+};
+
+export default class CardEdit extends Component {
+	state = {
+		status: status.INIT,
+		editLang: "en",
+		lang: [],
+		body: [],
+	};
+
+	async componentDidMount() {
+		this.setState({ status: status.READING });
+		const { path, name } = this.props;
+
+		try {
+			const cardData = await db
+				.collection(Collections.CARDS)
+				.find({ path, name })
+				.first();
+			console.log(cardData);
+			this.setState({
+				lang: cardData.lang,
+				body: cardData.body,
+				status: status.DONE,
+			});
+		} catch (error) {
+			console.error(error);
+			this.setStatus({ status: status.ERROR });
+		}
+	}
+
+	changeEditLang = (e) => {
+		const newEditLang = e.currentTarget.value;
+		this.setState({ editLang: newEditLang });
+	};
+
+	getContent = (lang) => {
+		let ctn = this.state.body[lang] || "";
+
+		return ctn;
+	};
+
+	setContent = (e) => {
+		const ctn = e.currentTarget.value;
+		const body = { ...this.state.body };
+		body[this.state.editLang] = ctn;
+		this.setState({ body });
+	};
+
+	render() {
+		const editLang = this.state.editLang;
+		const layoutLangsList = LayoutsStore.current.langs;
+		const cardLangsList = this.state.lang;
+		return (
+			<Window
+				className="window"
+				title={"Edit card: " + this.props.name}
+				onClose={this.props.onClose}
+			>
+				<label htmlFor="card-lang">Current language:</label>
+				<div className="justify-between">
+					<select
+						id="card-lang"
+						style={{ width: "100%" }}
+						value={editLang}
+						onChange={this.changeEditLang}
+					>
+						{layoutLangsList.length > 0 ? (
+							layoutLangsList.map((lang, index) => {
+								const isUsed = this.getContent(lang.symbol).trim() !== "";
+								return (
+									<option key={index} value={lang.symbol}>
+										{lang.name}
+										{isUsed ? "*" : ""}
+									</option>
+								);
+							})
+						) : (
+							<option key="no-lang">No language defined.</option>
+						)}
+					</select>
+					<button onClick={this.createLang}>Add...</button>
+				</div>
+				<div
+					className="d-flex flex-column justify-content-start align-items-start"
+					style={{ flexGrow: "2" }}
+				>
+					<label htmlFor="card-body">Body:</label>
+					<textarea
+						id="card-body"
+						value={this.getContent(editLang)}
+						onChange={this.setContent}
+						style={{ height: "100%" }}
+					/>
+				</div>
+				<div className="justify-right">
+					<button onClick={this.saveCard}>Save card</button>
+				</div>
+			</Window>
+		);
+	}
+}
---
+++ src/components/admin/windows/Window.jsx
@@ -0,0 +1,108 @@
+import "../scss/window.scss";
+
+import React, { Component } from "react";
+
+import * as Icon from "react-bootstrap-icons";
+// import CustomScrollbar from "../../layout/CustomScrollbar";
+
+export class WindowButton extends Component {
+	render() {
+		const ButtonIcon = this.props.icon ? Icon[this.props.icon] : null;
+		return (
+			<button disabled={!Boolean(ButtonIcon)} onClick={this.props.onClick}>
+				{Boolean(ButtonIcon) && <ButtonIcon size="16px" />}
+			</button>
+		);
+	}
+}
+
+export class WindowControl extends Component {
+	render() {
+		const { onMaximize, onClose, onMinimize, windowState } = this.props;
+		return (
+			<div>
+				{windowState !== "maximized" && onMaximize ? (
+					windowState === "minimized" ? (
+						<WindowButton icon="CaretUp" onClick={onMaximize} />
+					) : (
+						<WindowButton icon="CaretLeft" onClick={onMaximize} />
+					)
+				) : (
+					<WindowButton icon={null} />
+				)}
+				{onClose && <WindowButton icon="X" onClick={onClose} />}
+				{windowState !== "minimized" && onMinimize ? (
+					<WindowButton icon="CaretRight" onClick={onMinimize} />
+				) : null}
+			</div>
+		);
+	}
+}
+
+export default class Window extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			windowSize: "panel",
+		};
+	}
+
+	maximize = () => {
+		const { onMaximize } = this.props;
+
+		switch (this.state.windowSize) {
+			case "minimized":
+				this.setState({ windowSize: "panel" });
+				break;
+			default:
+				this.setState({ windowSize: "maximized" });
+				if (onMaximize) onMaximize();
+				break;
+		}
+	};
+
+	minimize = () => {
+		const { onMinimize } = this.props;
+
+		switch (this.state.windowSize) {
+			case "maximized":
+				this.setState({ windowSize: "panel" });
+				break;
+			default:
+				this.setState({ windowSize: "minimized" });
+				if (onMinimize) onMinimize();
+				break;
+		}
+	};
+
+	render() {
+		const { className, title, onClose, children } = this.props;
+
+		const windowSize = this.state.windowSize;
+		let windowStateClass = "";
+		switch (windowSize) {
+			case "minimized":
+				windowStateClass = " minimized";
+				break;
+			case "maximized":
+				windowStateClass = " maximized";
+				break;
+			default:
+				windowStateClass = "";
+		}
+		return (
+			<div className={className + windowStateClass}>
+				<div className="header">
+					<span title={title}>{title}</span>
+					<WindowControl
+						windowState={windowSize}
+						onMinimize={this.minimize}
+						onMaximize={this.maximize}
+						onClose={onClose}
+					/>
+				</div>
+				{windowSize !== "minimized" && <form>{children}</form>}
+			</div>
+		);
+	}
+}
