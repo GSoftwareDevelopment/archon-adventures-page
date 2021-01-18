ce16127dd7bc7d33d76ab07fc9537394a51aaa42
GhostSD <ghostsoftwaredevelopment@gmail.com>
2021-01-18 18:03:38 +0100

    Update component

--- src/components/layout/Alert.jsx
+++ src/components/layout/Alert.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
-import * as Icon from "react-bootstrap-icons";
+import { X as HideIcon } from "react-bootstrap-icons";
 
 export default function Alert({ variant, className, onHide, ...props }) {
 	return (
 		<div className={className + " " + variant} {...props}>
-			<button onClick={onHide}>
-				<Icon.X size="16px" />
-			</button>
+			{onHide && (
+				<button className="close" onClick={onHide}>
+					<HideIcon size="16px" />
+				</button>
+			)}
 			{props.children}
 		</div>
 	);
