aff3ee4c23faeb5187ad19a648478ccaac3af492
GhostSD <ghostsoftwaredevelopment@gmail.com>
2021-01-27 18:49:29 +0100

    update

--- src/components/admin/menu.js
+++ src/components/admin/menu.js
@@ -1,12 +1,37 @@
+import * as Icon from "react-bootstrap-icons";
+
 import TreeLayouts from "./TreeLayouts";
 import TreeCards from "./TreeCards";
 import TreeCalendars from "./TreeCalendars";
 import TreeGalleries from "./TreeGalleries";
 
+const ICON_SIZE = 20;
+export const SIZE_PROP = ICON_SIZE + "px";
+
 export const MenuRoot = [
-	{ icon: "LayoutWtf", name: "Layouts", Component: TreeLayouts },
-	{ icon: "Journals", name: "Cards", Component: TreeCards },
-	{ icon: "Calendar3", name: "Calendars", Component: TreeCalendars },
-	{ icon: "Images", name: "Galleries", Component: TreeGalleries },
-	{ icon: "ChatRightText", name: "Comments", Component: null },
+	{
+		icon: <Icon.LayoutWtf size={SIZE_PROP} />,
+		name: "Layouts",
+		Component: TreeLayouts,
+	},
+	{
+		icon: <Icon.Journals size={SIZE_PROP} />,
+		name: "Cards",
+		Component: TreeCards,
+	},
+	{
+		icon: <Icon.Calendar3 size={SIZE_PROP} />,
+		name: "Calendars",
+		Component: TreeCalendars,
+	},
+	{
+		icon: <Icon.Images size={SIZE_PROP} />,
+		name: "Galleries",
+		Component: TreeGalleries,
+	},
+	{
+		icon: <Icon.ChatRightText size={SIZE_PROP} />,
+		name: "Comments",
+		Component: null,
+	},
 ];
