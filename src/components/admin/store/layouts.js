69264eda45c28bd315d36bb6fa986d8a478ae9cb
GhostSD <ghostsoftwaredevelopment@gmail.com>
2021-01-31 18:21:49 +0100

    New DB version - more in /src/lib/db.md
    New 'ContentTypes' constant. Add new methods 'getElementById' and 'moveItemInNode'.

--- src/components/admin/store/layouts.js
+++ src/components/admin/store/layouts.js
@@ -9,25 +9,46 @@ import {
 import { Collections } from "../../../setup";
 import { db } from "../../../libs/db";
 
-export const status = {
+export const Status = {
 	INIT: "init",
 	PENDING: "pending",
 	DONE: "done",
 	ERROR: "error",
 };
 
+export const ContentTypes = {
+	LAYOUT: "layout",
+
+	// container blocks types
+	MENULINK: "menu-link",
+	ROUTERCONTENT: "router-content",
+
+	// content blocks types
+	LANGSELECTOR: "lang-selector",
+	CARD: "card",
+	CALENDAR: "calendar",
+	GALERY: "galery",
+	COMMENTS: "comments",
+};
+
 class LayoutsStore {
-	status = status.INIT;
+	status = Status.INIT;
 	layoutsList = [];
+	elements = [];
 
 	constructor() {
 		makeObservable(this, {
 			status: observable,
 			layoutsList: observable,
-			default: computed,
-			current: computed,
+			elements: observable,
+			//
 			currentStatus: computed,
 			available: computed,
+			default: computed,
+			current: computed,
+			//
+			getElementById: action,
+			moveItemInNode: action,
 			fetchList: action,
 		});
 	}
@@ -48,28 +69,82 @@ class LayoutsStore {
 		return this.layoutsList.find((layout) => layout.current);
 	}
 
+	getElementById(id) {
+		return this.elements.find((el) => el._id === id);
+	}
+
+	moveItemInNode(nodeId, itemIndex, direction) {
+		const nodeChilds = this.getElementById(nodeId).childs;
+
+		if (direction < 0) {
+			const itemSrc = nodeChilds[itemIndex];
+			const itemDst = nodeChilds[itemIndex - 1];
+			nodeChilds[itemIndex - 1] = itemSrc;
+			nodeChilds[itemIndex] = itemDst;
+		} else if (direction > 0) {
+			const itemSrc = nodeChilds[itemIndex];
+			const itemDst = nodeChilds[itemIndex + 1];
+			nodeChilds[itemIndex + 1] = itemSrc;
+			nodeChilds[itemIndex] = itemDst;
+		}
+	}
+
 	async fetchList() {
-		this.status = status.PENDING;
+		this.status = Status.PENDING;
 		try {
-			const layouts = await db
+			const elements = await db
 				.collection(Collections.LAYOUT)
 				.find({})
 				.asArray();
 
 			runInAction(() => {
-				layouts.forEach((layout) => {
-					layout.default = layout.current;
-				});
+				this.elements = elements.map((el) => ({
+					...el,
+					_id: el._id.toString(),
+				}));
+				this.layoutsList = this.elements.filter(
+					(el) => el.contentType === ContentTypes.LAYOUT
+				);
 
-				this.layoutsList = layouts;
-
-				this.status = status.DONE;
+				this.status = Status.DONE;
 			});
 		} catch (error) {
 			console.error(error);
-			this.status = status.ERROR;
+			this.status = Status.ERROR;
 		}
 	}
+
+	// async migrate(item, parrentId = null) {
+	// 	let { _id, scheme, elements, ___collapsed, ...elProps } = item;
+
+	// 	if (scheme) elements = scheme;
+	// 	if (parrentId) elProps.parrent = parrentId;
+	// 	let childs = [];
+
+	// 	try {
+	// 		let result = await db.collection(Collections.LAYOUT).insertOne(elProps);
+	// 		_id = result.insertedId;
+	// 		console.log(_id);
+	// 		if (elements) {
+	// 			const promises = elements.map(
+	// 				(item) =>
+	// 					new Promise((resolve, reject) => resolve(this.migrate(item, _id)))
+	// 			);
+	// 			childs = await Promise.all(promises);
+
+	// 			console.log(childs);
+	// 			elProps.childs = childs;
+	// 			result = await db
+	// 				.collection(Collections.LAYOUT)
+	// 				.updateOne({ _id }, elProps);
+
+	// 			console.log(result);
+	// 		}
+	// 		return _id;
+	// 	} catch (error) {
+	// 		console.error(error);
+	// 	}
+	// }
 }
 
 const _LayoutsStore = new LayoutsStore();
