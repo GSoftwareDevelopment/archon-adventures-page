e83ad49d7950b4ac94ef2f607caa9769b52c4112
GhostSD <ghostsoftwaredevelopment@gmail.com>
2021-01-31 18:34:18 +0100

    Extension of the responsibilities of the component.

--- src/components/admin/SidebarMenu.jsx
+++ src/components/admin/SidebarMenu.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 
+import CustomScrollbar from "../layout/CustomScrollbar";
+import { ButtonsGroup } from "./windows/Window";
+
 export default class SidebarMenu extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = {};
+		this.state = {
+			active: null,
+			options: [],
+		};
 		this.props.items.forEach((item) => {
 			const id = item.name.toLowerCase();
 			this.state[id] = false;
@@ -12,35 +18,77 @@ export default class SidebarMenu extends Component {
 	}
 
 	toggleNodeVisibility(id) {
+		this.setState({ active: id });
+		if (this.state.active === id) {
+			this.setState({ options: [] });
+		}
 		const old = this.state[id];
 		this.setState({ [id]: !old });
 	}
 
+	setOptions = (opt) => {
+		this.setState({ options: opt });
+	};
+
 	render() {
-		return this.props.items.map((item, index) => {
-			const id = item.name.toLowerCase();
-			const MenuComponent = item.Component;
-			return (
-				<React.Fragment key={index}>
-					<button
-						className="flat"
-						onClick={() => {
-							this.toggleNodeVisibility(id);
-							this.props.setOptions([]);
-						}}
-					>
-						{item.icon}
-						<span>{item.name}</span>
-					</button>
-					{item.Component && (
-						<MenuComponent
-							visible={this.state[id]}
-							onOpenWindow={this.props.onOpenWindow}
-							setOptions={this.props.setOptions}
-						/>
-					)}
-				</React.Fragment>
-			);
-		});
+		const currentActive = this.state.active;
+		return (
+			<React.Fragment>
+				<div className="sidebar-menu">
+					<CustomScrollbar>
+						<div style={{ display: "flex", flexDirection: "column" }}>
+							{this.props.items.map((item, index) => {
+								const id = item.name.toLowerCase();
+								const MenuComponent = item.Component;
+								return (
+									<React.Fragment key={index}>
+										<div
+											style={{
+												display: "flex",
+												flexDirection: "column",
+												borderLeft:
+													currentActive === id
+														? "#fff8 2px solid"
+														: "#fff0 2px solid",
+											}}
+										>
+											<button
+												className="flat"
+												onClick={() => {
+													this.toggleNodeVisibility(id);
+												}}
+											>
+												{item.icon}
+												<span>{item.name}</span>
+											</button>
+											{item.Component && (
+												<MenuComponent
+													visible={this.state[id]}
+													onOpenWindow={this.props.onOpenWindow}
+													setOptions={this.setOptions}
+													setActive={() => {
+														if (this.state.active !== id)
+															this.setState({ active: id });
+													}}
+												/>
+											)}
+										</div>
+									</React.Fragment>
+								);
+							})}
+						</div>
+					</CustomScrollbar>
+				</div>
+				{Boolean(this.state.options?.length) && (
+					<ButtonsGroup
+						className={
+							"options-group-button" + (this.props.visible ? " isExpanded" : "")
+						}
+						onlyIcons={true}
+						buttons={this.state.options}
+					/>
+				)}
+			</React.Fragment>
+		);
 	}
 }
