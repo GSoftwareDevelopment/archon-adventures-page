c0e0d6c433a071515c1b3f2e38aab31e4110960d
GhostSD <ghostsoftwaredevelopment@gmail.com>
2021-02-02 16:56:01 +0100

    dnd components from https://github.com/ChrisDobby/react-drag-drop-demo

---
+++ src/components/admin/dropEffects.js
@@ -0,0 +1,12 @@
+/*
+		This code has taken from https://github.com/ChrisDobby/react-drag-drop-demo
+*/
+
+export const All = "all";
+export const Move = "move";
+export const Copy = "copy";
+export const Link = "link";
+export const CopyOrMove = "copyMove";
+export const CopyOrLink = "copyLink";
+export const LinkOrMove = "linkMove";
+export const None = "none";
