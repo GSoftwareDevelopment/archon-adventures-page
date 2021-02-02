53f11b16b13d2d54faf6e854ee39804c815ea66e
GhostSD <ghostsoftwaredevelopment@gmail.com>
2021-02-02 09:59:45 +0100

    Putting the definition of the layout elements in a separate file. And other minor changes.

--- src/components/admin/TreeLayouts.jsx
+++ src/components/admin/TreeLayouts.jsx
@@ -1,23 +1,18 @@
-import React, { Component, useEffect } from "react";
+import React, { Component } from "react";
 import { observer } from "mobx-react";
 import WindowsStore from "./store/windows";
 import LayoutsStore, { Status, ContentTypes } from "./store/layouts";
 
 import NodeTree from "./NodeTree";
 import NodeItem from "./NodeItem";
+import { treeItems } from "./ContentType";
 
 import Spinner from "react-spinners/DotLoader";
 
 import {
-	Link45deg as IconRouterContent,
-	Link as IconMenuLink,
-	List as IconLangSelector,
-	JournalRichtext as IconCard,
-	Calendar3 as IconCalendar,
-	JournalAlbum as IconGallery,
-	ChatRight as IconComment,
-	//
+	// option icons
 	PlusCircle as IconCreateLayout,
+	ArrowRepeat as IconRefresh,
 	Sliders as IconLayoutProps,
 	NodePlus as IconAddElement,
 	ArrowBarUp as IconMoveBefore,
@@ -25,18 +20,13 @@ import {
 	Trash as IconTrash,
 } from "react-bootstrap-icons";
 
-import PropsOfLayout from "./windows/PropsOfLayout";
-import { unifyPath } from "../../libs/utils";
-import { Path } from "../../setup";
-
 class TreeLayouts extends Component {
 	state = {
 		selected: null,
 	};
 
 	async componentDidMount() {
-		console.log("> Getting layouts list...");
-		await LayoutsStore.fetchList();
+		await this.getLayoutData();
 	}
 
 	componentDidUpdate(prevProps) {
@@ -46,25 +36,35 @@ class TreeLayouts extends Component {
 				{
 					icon: <IconCreateLayout size="20px" />,
 					style: { marginRight: "auto" },
-					title: "Create new layout",
+					title: "New layout...",
+					tip: "Create new layout",
+				},
+				{
+					icon: <IconRefresh size="20px" />,
+					tip: "Refresh",
+					onClick: () => {
+						this.getLayoutData();
+					},
 				},
 			]);
 		}
-		console.log("TreeLayout update...");
 	}
 
-	openLayoutEdit(layout) {
-		const { scheme, ...onlyLayoutAttr } = layout;
-		WindowsStore.addWindow(
-			layout._id.toString(),
-			PropsOfLayout,
-			onlyLayoutAttr
-		);
-		// this.props.onOpenWindow();
+	async getLayoutData() {
+		console.log("> Getting layouts data...");
+		await LayoutsStore.fetchList();
 	}
 
 	openElementProps = (item) => {
 		console.log(`Property of '${item.contentType}' on ID#${item.id}...`);
+		const { parrent, childs, ...itemAttr } = item;
+		const wnd = treeItems[item.contentType]?.elementProps;
+		if (wnd) WindowsStore.addWindow(item._id.toString(), wnd, itemAttr);
+		else {
+			console.error(
+				`! Class for window properties of '${item.contentType}' is not defined.`
+			);
+		}
 	};
 
 	doSelect = (item) => {
@@ -73,55 +73,59 @@ class TreeLayouts extends Component {
 
 		this.props.setActive();
 
-		let nodeChilds = [];
+		let parentNode = null;
+		let parentNodeChilds = [];
 		if (item.parrent) {
 			const parentNodeId = item.parrent.toString();
-			nodeChilds = LayoutsStore.getElementById(parentNodeId)?.childs;
+			parentNode = LayoutsStore.getElementById(parentNodeId);
+			parentNodeChilds = parentNode?.childs;
 		}
 
-		const itemIndex = nodeChilds.findIndex(
+		const itemIndex = parentNodeChilds.findIndex(
 			(item) => itemId === item.toString()
 		);
 
 		this.props.setOptions([
 			{
+				icon: <IconAddElement size="20px" />,
+				title: "Add",
+				tip: `Add new element in '${item.contentType}' node`,
+			},
+			{
 				icon: <IconLayoutProps size="20px" />,
-				title: "Properties",
+				title: "Props...",
+				tip: `Edit '${item.contentType}' properties`,
 				onClick: () => this.openElementProps(item),
 			},
 			{
-				icon: <IconAddElement size="20px" />,
-				title: "Add",
-			},
-			{
 				icon: <IconMoveBefore size="20px" />,
-				title: "Move Up",
+				tip: "Move Up",
 				onClick: () => {
 					LayoutsStore.moveItemInNode(item.parrent.toString(), itemIndex, -1);
 					this.doSelect(item);
 				},
-				enabled: nodeChilds?.length && itemIndex > 0,
+				enabled: parentNodeChilds?.length && itemIndex > 0,
 				visible: item.contentType !== ContentTypes.LAYOUT,
 			},
 			{
 				icon: <IconMoveAfter size="20px" />,
-				title: "Move Down",
+				tip: "Move Down",
 				onClick: () => {
 					LayoutsStore.moveItemInNode(item.parrent.toString(), itemIndex, 1);
 					this.doSelect(item);
 				},
-				enabled: nodeChilds?.length && itemIndex < nodeChilds?.length - 1,
+				enabled:
+					parentNodeChilds?.length && itemIndex < parentNodeChilds?.length - 1,
 				visible: item.contentType !== ContentTypes.LAYOUT,
 			},
 			{
 				icon: <IconTrash size="20px" style={{ color: "#f00" }} />,
 				style: { marginLeft: "auto" },
-				title: "Delete",
+				tip: "Delete",
 			},
 		]);
 
 		if (this.state.selected === itemId) return true;
-		// console.log(item);
 	};
 
 	render() {
@@ -154,11 +158,8 @@ export default observer(TreeLayouts);
 
 const ElementsList = observer(
 	({ id, list, selected, onClick, onDoubleClick, ...props }) => {
-		useEffect(() => {
-			console.log(id);
-		});
 		if (!list) {
-			console.error("TreeLayouts component error. No source data.", this.props);
+			console.error("TreeLayouts list data is not defined.");
 			return null;
 		}
 
@@ -207,96 +208,3 @@ const ElementsList = observer(
 		});
 	}
 );
-
-// class ElementsList extends Component {
-// 	render() {
-// 		if (!this.props.list) {
-// 			console.error("TreeLayouts component error. No source data.", this.props);
-// 			return null;
-// 		}
-// 		return this.props.list.map((id) => {
-// 			id = id.toString();
-// 			const element = LayoutsStore.getElementById(id);
-// 			if (!element) {
-// 				console.error(`! Element #${id} can't be found`);
-// 				return null;
-// 			}
-
-// 			const haveChildrens = typeof element.childs !== "undefined";
-// 			let icon = null;
-// 			let title = element.contentType;
-
-// 			const ES = treeItems[element.contentType];
-// 			if (ES) {
-// 				if (ES.icon) icon = ES.icon;
-// 				if (ES.title) title = ES.title(element);
-// 			}
-
-// 			return (
-// 				<NodeItem
-// 					key={id}
-// 					icon={icon}
-// 					title={title}
-// 					selected={this.props.selected === id}
-// 					onClick={() => {
-// 						if (this.props.onClick) this.props.onClick(element);
-// 					}}
-// 					onDoubleClick={() => {
-// 						if (this.props.onDoubleClick) this.props.onDoubleClick(element);
-// 					}}
-// 				>
-// 					{haveChildrens && (
-// 						<ElementsList
-// 							id={id}
-// 							list={element.childs}
-// 							selected={this.props.selected}
-// 							onClick={this.props.onClick}
-// 							onDoubleClick={this.props.onDoubleClick}
-// 						/>
-// 					)}
-// 				</NodeItem>
-// 			);
-// 		});
-// 	}
-// }
-
-//
-
-const SIZE_PROP = "20px";
-const treeItems = {
-	layout: {
-		icon: null,
-		title: ({ name }) => <div style={{ fontWeight: "bold" }}>{name}</div>,
-	},
-	"menu-link": {
-		icon: <IconMenuLink size={SIZE_PROP} />,
-		title: ({ id }) => <div style={{ fontStyle: "italic" }}>{id}</div>,
-	},
-	"lang-selector": {
-		icon: <IconLangSelector size={SIZE_PROP} />,
-	},
-	card: {
-		icon: <IconCard size={SIZE_PROP} />,
-		title: ({ name }) => (
-			<div>
-				Card{" "}
-				<span style={{ fontStyle: "italic" }}>
-					{Path.DELIMITER + unifyPath(name)}
-				</span>
-			</div>
-		),
-	},
-	calendar: {
-		icon: <IconCalendar size={SIZE_PROP} />,
-	},
-	galery: {
-		icon: <IconGallery size={SIZE_PROP} />,
-	},
-	comments: {
-		icon: <IconComment size={SIZE_PROP} />,
-	},
-	"router-content": {
-		icon: <IconRouterContent size={SIZE_PROP} />,
-		title: ({ id }) => <span style={{ fontStyle: "italic" }}>{id}</span>,
-	},
-};
