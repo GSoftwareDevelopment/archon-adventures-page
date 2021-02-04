198ee8d9a40753ecf2805dc460381c7ea5b0dd0c
GhostSD <ghostsoftwaredevelopment@gmail.com>
2021-02-04 17:22:57 +0100

    Update after LayoutsStores merger

--- src/components/layout/PageLayout.jsx
+++ src/components/layout/PageLayout.jsx
@@ -1,22 +1,22 @@
 import { Component } from "react";
 import { observer } from "mobx-react";
-import LayoutsStore, { status } from "../../store/layouts";
+import LayoutsStore, { Status } from "../../store/layouts";
 
 import { EmojiFrown } from "react-bootstrap-icons";
 import { parseElements } from "../page-elements/parse";
 
 class PageLayout extends Component {
 	async componentDidMount() {
-		await LayoutsStore.fetchGetLayout({ current: true });
+		await LayoutsStore.fetchList();
 	}
 
 	render() {
-		switch (LayoutsStore.getStatus()) {
-			case status.DONE:
-				const rootElements = LayoutsStore.getSchema();
+		switch (LayoutsStore.currentStatus) {
+			case Status.DONE:
+				const rootElements = LayoutsStore.default.childs;
 				return <div id="layout">{parseElements("Root", rootElements)}</div>;
-			case status.INIT:
-			case status.PENDING:
+			case Status.INIT:
+			case Status.PENDING:
 				return <div className="content-loader">Loading...</div>;
 			default:
 				return (
