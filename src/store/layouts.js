c3673d713350d2fe62172e6c2c4f0fd0b29321d8
GhostSD <ghostsoftwaredevelopment@gmail.com>
2021-02-04 17:15:01 +0100

    Merger of two Stores - LayoutsStore from the Admin & LayoutsStore part of the Pages

--- src/store/layouts.js
+++ src/store/layouts.js
@@ -1,100 +1,174 @@
-import { makeAutoObservable, runInAction } from "mobx";
+import {
+	action,
+	computed,
+	makeObservable,
+	observable,
+	runInAction,
+} from "mobx";
+
 import { Collections } from "../setup";
 import { db } from "../libs/db";
 
-export const status = {
+//
+
+export const Status = {
 	INIT: "init",
 	PENDING: "pending",
 	DONE: "done",
 	ERROR: "error",
 };
 
+export const ContentTypes = {
+	LAYOUT: "layout",
+
+	// container blocks types
+	MENULINK: "menu-link",
+	ROUTERCONTENT: "router-content",
+
+	// content blocks types
+	LANGSELECTOR: "lang-selector",
+	CARD: "card",
+	CALENDAR: "calendar",
+	GALERY: "galery",
+	COMMENTS: "comments",
+};
+
 class LayoutsStore {
-	status = "init";
-	schema = [];
-	langs = [];
-	defaultLang = "";
+	status = Status.INIT;
+	layoutsList = [];
+	elements = [];
 	currentLang = "";
 
 	constructor() {
-		makeAutoObservable(this);
+		makeObservable(this, {
+			status: observable,
+			layoutsList: observable,
+			elements: observable,
+			currentLang: observable,
+			//
+			currentStatus: computed,
+			available: computed,
+			default: computed,
+			current: computed,
+			getCurrentLang: computed,
+			//
+			setCurrentLang: action,
+			getElementById: action,
+			moveItemInNode: action,
+			fetchList: action,
+		});
 	}
 
-	getStatus() {
+	get currentStatus() {
 		return this.status;
 	}
 
-	getSchema() {
-		return this.schema;
+	get available() {
+		return this.layoutsList;
 	}
 
-	getSchemaElements(contentType) {
-		const content = this.schema.find(
-			(contentItem) => contentItem.contentType === contentType
-		);
-		if (content && content.elements) return content.elements;
-		else return [];
+	get default() {
+		return this.layoutsList.find((layout) => layout.default);
 	}
 
-	getDefaultLang() {
-		return this.defaultLang;
+	get current() {
+		return this.layoutsList.find((layout) => layout.current);
 	}
 
-	getCurrentLang() {
+	get getCurrentLang() {
 		return this.currentLang;
 	}
 
 	setCurrentLang(lang) {
-		this.status = status.PENDING;
+		this.status = Status.PENDING;
 		runInAction(() => {
 			this.currentLang = lang;
-			this.status = status.DONE;
+			this.status = Status.DONE;
 		});
 	}
 
-	getAvailableLang() {
-		return this.langs;
+	getElementById(id) {
+		return this.elements.find((el) => el._id === id);
 	}
 
-	async fetchGetLayout(find) {
-		console.log("Fetching layout settings...");
-		this.status = status.PENDING;
-		try {
-			const layout = await db.collection(Collections.LAYOUT).find(find).first();
-			runInAction(() => {
-				if (layout) {
-					console.log(`Layout '${layout.name}' loaded.`);
-					this.schema = layout.scheme;
-					this.langs = layout.langs;
-					if (!this.langs || this.langs.length === 0) {
-						throw new Error("No languages defined!");
-					}
-					console.log(
-						"Defined languages: ",
-						this.langs.map((l) => l.symbol)
-					);
+	getElementsByContentType(rootNode, contentType) {}
 
-					this.defaultLang = layout.defaultLang;
-					if (!this.defaultLang) {
-						console.warn("No default language has been specified.");
-						this.defaultLang = this.langs[0].symbol;
-						console.warn("Language used: ", this.defaultLang);
-					}
-					this.currentLang = this.defaultLang;
-					this.status = status.DONE;
-				} else {
-					console.log(`Can't find current layout :/`);
-					this.schema = [];
-					this.status = status.ERROR;
-				}
-			});
-		} catch (error) {
-			this.schema = [];
-			this.status = status.ERROR;
-			console.error(error);
+	moveItemInNode(nodeId, itemIndex, direction) {
+		const nodeChilds = this.getElementById(nodeId).childs;
+
+		if (direction < 0) {
+			const itemSrc = nodeChilds[itemIndex];
+			const itemDst = nodeChilds[itemIndex - 1];
+			nodeChilds[itemIndex - 1] = itemSrc;
+			nodeChilds[itemIndex] = itemDst;
+		} else if (direction > 0) {
+			const itemSrc = nodeChilds[itemIndex];
+			const itemDst = nodeChilds[itemIndex + 1];
+			nodeChilds[itemIndex + 1] = itemSrc;
+			nodeChilds[itemIndex] = itemDst;
 		}
 	}
+
+	async fetchList() {
+		this.status = Status.PENDING;
+		try {
+			const elements = await db
+				.collection(Collections.LAYOUT)
+				.find({})
+				.asArray();
+
+			runInAction(() => {
+				this.elements = elements.map((el) => ({
+					...el,
+					_id: el._id.toString(),
+				}));
+				this.layoutsList = this.elements.filter(
+					(el) => el.contentType === ContentTypes.LAYOUT
+				);
+				const defaultLayout = this.layoutsList.find((layout) => layout.default);
+				defaultLayout.current = true;
+				this.currentLang = defaultLayout.defaultLang;
+
+				this.status = Status.DONE;
+			});
+		} catch (error) {
+			console.error(error);
+			this.status = Status.ERROR;
+		}
+	}
+
+	// async migrate(item, parrentId = null) {
+	// 	let { _id, scheme, elements, ___collapsed, ...elProps } = item;
+
+	// 	if (scheme) elements = scheme;
+	// 	if (parrentId) elProps.parrent = parrentId;
+	// 	let childs = [];
+
+	// 	try {
+	// 		let result = await db.collection(Collections.LAYOUT).insertOne(elProps);
+	// 		_id = result.insertedId;
+	// 		console.log(_id);
+	// 		if (elements) {
+	// 			const promises = elements.map(
+	// 				(item) =>
+	// 					new Promise((resolve, reject) => resolve(this.migrate(item, _id)))
+	// 			);
+	// 			childs = await Promise.all(promises);
+
+	// 			console.log(childs);
+	// 			elProps.childs = childs;
+	// 			result = await db
+	// 				.collection(Collections.LAYOUT)
+	// 				.updateOne({ _id }, elProps);
+
+	// 			console.log(result);
+	// 		}
+	// 		return _id;
+	// 	} catch (error) {
+	// 		console.error(error);
+	// 	}
+	// }
 }
 
-let layoutsStore = (window.layoutsStore = new LayoutsStore());
-export default layoutsStore;
+const _LayoutsStore = new LayoutsStore();
+export default _LayoutsStore;
