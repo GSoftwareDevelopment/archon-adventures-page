10c4d2faf199dec74fec7d0241489e7f2c43a239
GhostSD <ghostsoftwaredevelopment@gmail.com>
2021-02-16 14:02:16 +0100

    Update (more in [documentation](/doc/db.md#v2.3))

--- src/components/layout/page-elements/MenuLink.jsx
+++ src/components/layout/page-elements/MenuLink.jsx
@@ -1,29 +1,72 @@
 import "./scss/menu-link.scss";
 import { observer } from "mobx-react";
 
-import LayoutsStore from "../../../store/layouts";
+import LayoutsStore, { ContentTypes } from "../../../store/layouts";
 import { languageCheck } from "../../../libs/utils";
+import { Path } from "../../../setup";
 
 import * as Messages from "../../../libs/Messages";
 import { Link } from "react-router-dom";
 
-function MenuLink(props) {
+function MenuLink({ routes, attr, ...props }) {
 	const currentLayout = LayoutsStore.current;
 	const defaultLang = currentLayout.defaultLang;
 	const availableLangs = currentLayout.langs;
 	const currentLang = LayoutsStore.getCurrentLang;
 
-	const route = props.routes.find((entry) => entry.id === props.attr.id);
-	if (!route) {
-		return null;
+	let destRoute = attr.destRoute?.trim();
+	if (!destRoute) {
+		// for back compatibility
+		const route = routes?.find((entry) => entry.id === attr.id);
+		if (route) {
+			destRoute = route.path.trim();
+		} else {
+			console.log("Route ID#%o not exist", attr.id);
+			return;
+		}
 	}
+
+	const _parts = destRoute.split("/");
+	const routesData = LayoutsStore.getElementsByContentType(
+		ContentTypes.ROUTERCONTENT
+	).map((element) => ({
+		id: element.id,
+		path: element.path,
+	}));
+
+	let resultPath = [];
+
+	for (let dir of _parts) {
+		if (dir[0] === "#") {
+			let id = dir.slice(1);
+			const route = routesData.find((entry) => entry.id === id);
+			if (!route) {
+				console.log("Route ID#%o not exist", id);
+				return;
+			}
+			if (!route.path.trim()) {
+				console.log("Route path in ID#%o is not defined", id);
+				return;
+			}
+			let _routePath = route.path;
+			if (_routePath[0] === "/") _routePath = _routePath.slice(1);
+			if (_routePath[_routePath.length - 1] === "/")
+				_routePath = _routePath.slice(0, _routePath.length - 1);
+			if (_routePath.trim()) resultPath.push(_routePath);
+			continue;
+		}
+
+		if (dir.trim()) resultPath.push(dir);
+	}
+
+	destRoute = "/" + resultPath.join("/");
 
 	const usedLang = languageCheck(
 		currentLang,
 		defaultLang,
 		availableLangs.map((l) => l.symbol),
 		(lang) => {
-			if (props.attr.title) return props.attr.title[lang];
+			if (attr.title) return attr.title[lang];
 			return null;
 		}
 	);
@@ -36,20 +79,34 @@ function MenuLink(props) {
 		return null;
 	}
 
-	if (props.attr._parentContentType === "router-menu")
-		return (
-			<li className="menu-link">
-				<Link to={route.path} onClick={props.onClick}>
-					{props.attr.title[usedLang]}
-				</Link>
-			</li>
-		);
-	else
-		return (
-			<Link className="menu-link" to={route.path} onClick={props.onClick}>
-				{props.attr.title[usedLang]}
+	return (
+		<ConditionalWrapper
+			condition={attr._parentContentType === "router-menu"}
+			wrapper={(children) => <li className="menu-link">{children}</li>}
+		>
+			<Link to={destRoute} onClick={props.onClick}>
+				{attr.title[usedLang]}
 			</Link>
-		);
+		</ConditionalWrapper>
+	);
+
+	// if (attr._parentContentType === "router-menu")
+	// 	return (
+	// 		<li className="menu-link">
+	// 			<Link to={destRoute} onClick={props.onClick}>
+	// 				{attr.title[usedLang]}
+	// 			</Link>
+	// 		</li>
+	// 	);
+	// else
+	// 	return (
+	// 		<Link className="menu-link" to={route.path} onClick={props.onClick}>
+	// 			{attr.title[usedLang]}
+	// 		</Link>
+	// 	);
 }
 
 export default observer(MenuLink);
+
+const ConditionalWrapper = ({ condition, wrapper, children }) =>
+	condition ? wrapper(children) : children;
