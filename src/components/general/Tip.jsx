fb88f90ae402b8ace411f21175c889b576373d8a
GhostSD <ghostsoftwaredevelopment@gmail.com>
2021-02-28 10:30:39 +0100

    Using the 'rc-tooltip' library to display tooltip balloons.

--- src/components/general/Tip.jsx
+++ src/components/general/Tip.jsx
@@ -1,14 +1,67 @@
+import "rc-tooltip/assets/bootstrap.css";
 import "../../scss/tip.scss";
-import React, { Component } from "react";
+import React from "react";
+import Tooltip from "rc-tooltip";
 
 import { InfoCircle as IconHelp } from "react-bootstrap-icons";
+import MarkdownView from "react-showdown";
 
-export default class Tip extends Component {
-	render() {
-		return (
-			<span className="tip" title={this.props.title}>
+// let tipId;
+
+export default function Tip({ title, content }) {
+	// tipId = React.useRef(null);
+
+	const tipText = (
+		<div className="tip">
+			<div className="tip-header">
+				<IconHelp size="2em" />
+				<div className="tip-title">{title || "Information Tip"}</div>
+			</div>
+			<MarkdownView className="tip-content" markdown={content} />
+		</div>
+	);
+
+	return (
+		<Tooltip
+			placement="bottom"
+			trigger={["click"]}
+			overlay={tipText}
+			destroyTooltipOnHide={{ keepParent: false }}
+		>
+			<span className="tip-button">
 				<IconHelp />
 			</span>
-		);
-	}
+		</Tooltip>
+	);
 }
+
+/*
+		<span
+			className="tip-button"
+			onClick={(e) => {
+				if (tipId.current) toast.dismiss(tipId.current);
+				tipId.current = toast.dark(
+					<div className="tip">
+						<div className="tip-header">
+							<IconHelp size="2em" />
+							<div className="tip-title">{title || "Information Tip"}</div>
+						</div>
+						<MarkdownView className="tip-content" markdown={content} />
+						<div className="tip-footer">Click on tip to close</div>
+					</div>,
+					{
+						position: "top-center",
+						autoClose: false,
+						hideProgressBar: true,
+						closeOnClick: true,
+						pauseOnHover: true,
+						draggable: true,
+						progress: undefined,
+						closeButton: false,
+					}
+				);
+			}}
+		>
+			<IconHelp />
+		</span>
+*/
