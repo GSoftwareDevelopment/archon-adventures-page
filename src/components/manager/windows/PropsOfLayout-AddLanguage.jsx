ffb3ef573cf7af28fd9f9d48e658dd49b9d84158
GhostSD <ghostsoftwaredevelopment@gmail.com>
2021-02-28 10:37:35 +0100

    Dialog for adding new language support to the Layout.

---
+++ src/components/manager/windows/PropsOfLayout-AddLanguage.jsx
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+
+// import * as Messages from "../../../libs/Messages";
+import * as Icon from "react-bootstrap-icons";
+
+import { ButtonsGroup, Input } from "../../general/Window";
+
+export default class AddLanguage extends Component {
+	state = {
+		symbol: "",
+		name: "",
+		error: false,
+	};
+
+	constructor(props) {
+		super(props);
+
+		const { dialog } = props;
+
+		dialog({
+			size: "panel",
+			sizeCycle: ["panel"],
+			title: "Add language",
+			disableMaximize: true,
+			disableMinimize: true,
+		});
+	}
+
+	setSymbol = (e) => {
+		this.setState({ symbol: e.currentTarget.value });
+	};
+	setName = (e) => {
+		this.setState({ name: e.currentTarget.value });
+	};
+
+	onAdd = (e) => {
+		const { symbol, name } = this.state;
+		if (
+			symbol.trim() === "" ||
+			symbol.length > 2 ||
+			name.trim() === "" ||
+			name.length < 3
+		) {
+			this.setState({ error: true });
+		}
+
+		this.props.attr.actions[0](this.state); // TODO:	Niepodoba mi się ta forma wywołania akcji :/
+		this.props.close();
+	};
+
+	render() {
+		return (
+			<React.Fragment>
+				<Input
+					className="hover justify-between"
+					noWrapLabel
+					inputStyle={{ width: "50px", textAlign: "center" }}
+					name="lang_symbol"
+					label="Symbol (max. 2 chars)"
+					value={this.state.symbol}
+					onChange={this.setSymbol}
+					autoFocus
+				/>
+				<Input
+					className="hover justify-between"
+					noWrapLabel
+					name="lang_name"
+					label="Name"
+					value={this.state.name}
+					onChange={this.setName}
+				/>
+				<ButtonsGroup
+					className="group-button justify-content-end"
+					buttons={[
+						{
+							icon: <Icon.Check2 size="1.5em" />,
+							title: "Add",
+							onClick: this.onAdd,
+						},
+						{
+							icon: <Icon.X size="1.5em" />,
+							title: "Cancel",
+							onClick: this.props.close,
+						},
+					]}
+				/>
+			</React.Fragment>
+		);
+	}
+}
