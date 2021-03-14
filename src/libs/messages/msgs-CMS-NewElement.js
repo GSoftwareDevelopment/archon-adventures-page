ca590d2059952331e50cbe91b4fac55e098e3430
GhostSD <ghostsoftwaredevelopment@gmail.com>
2021-03-14 12:18:19 +0100

    update

--- src/libs/messages/msgs-CMS-NewElement.js
+++ src/libs/messages/msgs-CMS-NewElement.js
@@ -7,8 +7,7 @@ const base = "window.newElement";
 create(`${base}.info`, {
 	en: `
 Select an item from the list below that you want to add and
-
-drag it to the desired place in the Layouts list (in Sidebar), or...
+drag it to the desired place in the Layouts list (in Sidebar)
 `,
 });
 
