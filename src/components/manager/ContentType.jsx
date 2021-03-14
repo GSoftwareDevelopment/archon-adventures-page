04e625767c3062f4fcb1af3ab500590915043097
GhostSD <ghostsoftwaredevelopment@gmail.com>
2021-03-14 12:16:12 +0100

    update

--- src/components/manager/ContentType.jsx
+++ src/components/manager/ContentType.jsx
@@ -3,7 +3,8 @@ import { Path } from "../../setup";
 
 import {
 	// item icons
-	Link45deg as IconRouterContent,
+	Diagram3 as IconLayout,
+	Geo as IconRouterContent,
 	Link as IconMenuLink,
 	List as IconLangSelector,
 	JournalRichtext as IconCard,
@@ -18,59 +19,39 @@ import PropsOfMenuLink from "./windows/PropsOfMenuLink";
 import PropsOfCard from "./windows/PropsOfCard";
 import PropsOfRouterContent from "./windows/PropsOfRouterContent";
 
-const SIZE_PROP = "1.5em";
 export const treeItems = {
 	layout: {
-		icon: null,
-		title: ({ name, ...attr }) => (
-			<div style={{ fontWeight: "bold" }}>
-				{name}
-				{attr.default && <span> (default)</span>}
-			</div>
-		),
+		icon: IconLayout,
+		title: ({ name, ...attr }) => name + (attr.default ? " (default)" : ""),
 		elementProps: PropsOfLayout,
 	},
 	"menu-link": {
-		icon: <IconMenuLink size={SIZE_PROP} />,
-		title: ({ destRoute, id }) => (
-			<div style={{ fontStyle: "italic" }}>{destRoute || id}</div>
-		),
+		icon: IconMenuLink,
+		title: ({ destRoute, id }) => destRoute || id,
 		elementProps: PropsOfMenuLink,
 	},
 	"lang-selector": {
-		icon: <IconLangSelector size={SIZE_PROP} />,
+		icon: IconLangSelector,
 	},
 	"router-content": {
-		icon: <IconRouterContent size={SIZE_PROP} />,
-		title: ({ id }) => <span style={{ fontStyle: "italic" }}>{id}</span>,
+		icon: IconRouterContent,
+		title: ({ path }) => path,
 		elementProps: PropsOfRouterContent,
 	},
 	card: {
-		icon: <IconCard size={SIZE_PROP} />,
-		title: ({ name }) => (
-			<span style={{ fontStyle: "italic" }}>
-				{Path.DELIMITER + unifyPath(name)}
-			</span>
-		),
+		icon: IconCard,
+		title: ({ name }) => Path.DELIMITER + unifyPath(name),
 		elementProps: PropsOfCard,
 	},
 	calendar: {
-		icon: <IconCalendar size={SIZE_PROP} />,
-		title: ({ path }) => {
-			if (path)
-				return (
-					<span style={{ fontStyle: "italic" }}>
-						{Path.DELIMITER + unifyPath(path)}
-					</span>
-				);
-			else return "Calendar element";
-		},
+		icon: IconCalendar,
+		title: ({ path }) => (path ? Path.DELIMITER + unifyPath(path) : "Calendar"),
 		elementProps: PropsOfCalendar,
 	},
 	galery: {
-		icon: <IconGallery size={SIZE_PROP} />,
+		icon: IconGallery,
 	},
 	comments: {
-		icon: <IconComment size={SIZE_PROP} />,
+		icon: IconComment,
 	},
 };
