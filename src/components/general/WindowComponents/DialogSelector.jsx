fd6b2e34109efff36cbc3e55f36e84bf57ca175b
GhostSD <ghostsoftwaredevelopment@gmail.com>
2021-03-17 18:17:11 +0100

    Cosmetic appearance of the component

--- src/components/general/WindowComponents/DialogSelector.jsx
+++ src/components/general/WindowComponents/DialogSelector.jsx
@@ -14,25 +14,29 @@ export default function DialogSelector({ windows }) {
 					const title = wnd.sets.title;
 					return (
 						<div key={wnd.id} className="window-header">
-							<button
-								onClick={(e) => {
-									WindowsStore.removeWindowById(wnd.id);
-								}}
-							>
-								<IconClose />
-							</button>
-							<button
-								className="no-focus"
-								style={{ flexGrow: "2" }}
-								onClick={action(() => {
-									WindowsStore.bringToFront(wnd.id);
-									if (wnd.sets.restoreSize)
-										wnd.sets.size = wnd.sets.restoreSize;
-								})}
-								title={title}
-							>
-								{title}
-							</button>
+							<div>
+								<button
+									onClick={(e) => {
+										WindowsStore.removeWindowById(wnd.id);
+									}}
+								>
+									<IconClose />
+								</button>
+							</div>
+							<span>
+								<button
+									className="no-focus"
+									style={{ flexGrow: "2" }}
+									onClick={action(() => {
+										WindowsStore.bringToFront(wnd.id);
+										if (wnd.sets.restoreSize)
+											wnd.sets.size = wnd.sets.restoreSize;
+									})}
+									title={title}
+								>
+									{title}
+								</button>
+							</span>
 						</div>
 					);
 				})
