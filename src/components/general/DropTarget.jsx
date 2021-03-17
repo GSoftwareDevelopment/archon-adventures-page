8cb0735ac24989952ce7391f8a0184f97c125c51
GhostSD <ghostsoftwaredevelopment@gmail.com>
2021-03-17 18:20:05 +0100

    Getting rid of the built-in style in favour of a css class

--- src/components/general/DropTarget.jsx
+++ src/components/general/DropTarget.jsx
@@ -6,11 +6,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import * as dropEffects from "./dropEffects";
 
-const insideStyle = {
-	backgroundColor: "#cccccc",
-	opacity: 0.5,
-};
-
 const DropTarget = (props) => {
 	const [isOver, setIsOver] = React.useState(false);
 
@@ -40,7 +35,9 @@ const DropTarget = (props) => {
 			onDrop={drop}
 			onDragEnter={dragEnter}
 			onDragLeave={dragLeave}
-			style={{ width: "100%", height: "100%", ...(isOver ? insideStyle : {}) }}
+			className={
+				(props.className ? props.className : "") + (isOver ? " hover" : "")
+			}
 		>
 			{props.children}
 		</div>
@@ -53,7 +50,7 @@ DropTarget.propTypes = {
 	children: PropTypes.oneOfType([
 		PropTypes.arrayOf(PropTypes.node),
 		PropTypes.node,
-	]).isRequired,
+	]),
 };
 
 DropTarget.defaultProps = {
